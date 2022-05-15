f4f5c8dac4fa509e7f25678d6e9885e2ec649da9
Marius Aruxandei <mariushow@gmail.com>
2022-05-15 23:39:20 +0300

    change input to work on searching

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+// const { default: axios } = require("axios");
+
 const API_KEY = "37f0afe1ca89fac4c1d8f7b18798c757";
 const BASE_URL = "https://api.themoviedb.org/3/movie/";
 const IMG_URL = "https://image.tmdb.org/t/p/original";
 
 const fetchData = async (searchResult) => {
-  const FULL_URL = `${BASE_URL}${document.querySelector("input").value}?api_key=${API_KEY}`;
-  const response = await axios.get(FULL_URL);
+  const page = 1;
+  const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=${page}&query=${searchResult}&include_adult=true`;
+  const response = await axios.get(SEARCH_URL);
   console.log(response.data);
 };
 
 const input = document.querySelector("input");
-input.addEventListener("input", (e) => {
-  fetchData(e.target.value);
-});
+let timeoutID;
+const onInput = (e) => {
+  if (timeoutID) {
+    clearTimeout(timeoutID);
+  }
+  timeoutID = setTimeout(() => {
+    fetchData(e.target.value);
+  }, 1000);
+};
+input.addEventListener("input", onInput);
