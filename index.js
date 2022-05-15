45c233226eb518b4fdc5b436e42b836ef89d6087
Marius Aruxandei <mariushow@gmail.com>
2022-05-15 21:56:40 +0300

    change input search from text to id and change api to tmdb

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fetchData = async (searchTerm) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "5a0bac7d",
-      s: searchTerm,
-    },
-  });
+const API_KEY = "37f0afe1ca89fac4c1d8f7b18798c757";
+const BASE_URL = "https://api.themoviedb.org/3/movie/";
+const IMG_URL = "https://image.tmdb.org/t/p/original";
+
+const fetchData = async (searchResult) => {
+  const FULL_URL = `${BASE_URL}${document.querySelector("input").value}?api_key=${API_KEY}`;
+  const response = await axios.get(FULL_URL);
   console.log(response.data);
 };
 
