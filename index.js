172e02c826c4573119979523ad959f5ceb27f671
Marius Aruxandei <mariushow@gmail.com>
2022-05-16 18:11:30 +0300

    add basic search

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-// const { default: axios } = require("axios");
+// const API_KEY = "37f0afe1ca89fac4c1d8f7b18798c757";
+// const BASE_URL = "https://api.themoviedb.org/3/movie/";
+// const IMG_URL = "https://image.tmdb.org/t/p/original";
 
-const API_KEY = "37f0afe1ca89fac4c1d8f7b18798c757";
-const BASE_URL = "https://api.themoviedb.org/3/movie/";
-const IMG_URL = "https://image.tmdb.org/t/p/original";
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("https://api.themoviedb.org/3/search/movie", {
+    params: {
+      api_key: "37f0afe1ca89fac4c1d8f7b18798c757",
+      query: searchTerm,
+      include_adult: false,
+    },
+  });
 
-const fetchData = async (searchResult) => {
-  const page = 1;
-  const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=${page}&query=${searchResult}&include_adult=true`;
-  const response = await axios.get(SEARCH_URL);
-  console.log(response.data);
+  // if (response.status === 404) {
+  //   return [];
+  // }
+  // console.log(response.status);
+
+  return response.data.results;
 };
 
 const input = document.querySelector("input");
-let timeoutID;
-const onInput = (e) => {
-  if (timeoutID) {
-    clearTimeout(timeoutID);
+
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value);
+  movies.forEach((movie) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <img src="https://image.tmdb.org/t/p/w300${movie.poster_path}" class="smaller">
+    <h1>${movie.original_title}</h1>
+    `;
+    document.querySelector("#target").appendChild(div);
+  });
+  if (movies.length === 0) {
+    alert("Nothing here");
   }
-  timeoutID = setTimeout(() => {
-    fetchData(e.target.value);
-  }, 1000);
 };
-input.addEventListener("input", onInput);
+
+input.addEventListener("input", debounce(onInput));
